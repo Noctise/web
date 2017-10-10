98f3fbf21b60fdcda32df690075a021fbc72f8a8
msi <jfl73513@163.com>
2017-10-10 08:40:44 +0800

    js day01 by msi

---
+++ from_ftp/7.javascript/day 01/day01/1_output.js
@@ -0,0 +1,13 @@
+/*
+ * 多行注释
+ * */
+//单行注释
+//在当前网页中写入——DOM
+document.write(
+  "<h1>Hello JavaScript from js file</h1>");
+document.write(
+  '<h3>Welcome to my web site from js file!</h3>');
+//弹出一个对话框，显示消息:——BOM
+alert("Hello JavaScript from js file");
+//向控制台输出一行记录:
+console.log("Hello console from js file");//——ES
---
+++ from_ftp/7.javascript/day 01/day01/4_const.js
@@ -0,0 +1,9 @@
+"use strict";
+//请用户输入半径r
+var r=2;//prompt("请输入半径");
+//定义常量PI
+const PI=3.14;
+//PI=1.14;//静默失败
+//计算面积
+console.log("面积:"+PI*r*r);
+//空白处右键->RUN 4_const.js
